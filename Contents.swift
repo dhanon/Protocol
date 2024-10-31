b66677b9671b0237951a3d0f79bfde67f702472e
“dhanon” <dhanon68@gmail.com>
2024-11-01 01:26:50 +0600

    Save local changes before pulling

--- Contents.swift
+++ Contents.swift
@@ -292,33 +292,34 @@ print("Full Name: \(obj.fullname())")
 print("Age Type: \(obj.ageType())")
 
 
-//Checking for Protocol Conformancex`
-//@objc protocol rectangle {
-//   var area: Double { get }
-//}
-//
-//@objc class Circle: rectangle {
-//   let pi = 3.1415927
-//   var radius: Double
-//   var area: Double { return pi * radius * radius }
-//   init(radius: Double) { self.radius = radius }
-//}
-//
-//@objc class result: rectangle {
-//   var area: Double
-//   init(area: Double) { self.area = area }
-//}
-//
-//class sides {
-//   var rectsides: Int
-//   init(rectsides: Int) { self.rectsides = rectsides }
-//}
-//let objects: [AnyObject] = [Circle(radius: 2.0),result(area: 198),sides(rectsides: 4)]
-//
-//for object in objects {
-//   if let objectWithArea = object as? rectangle {
-//      print("Area is \(objectWithArea.area)")
-//   } else {
-//      print("Rectangle area is not defined")
-//   }
-//}
+//Protocol Conformancex
+Checking for Protocol Conformancex`
+@objc protocol rectangle {
+   var area: Double { get }
+}
+
+@objc class Circle: rectangle {
+   let pi = 3.1415927
+   var radius: Double
+   var area: Double { return pi * radius * radius }
+   init(radius: Double) { self.radius = radius }
+}
+
+@objc class result: rectangle {
+   var area: Double
+   init(area: Double) { self.area = area }
+}
+
+class sides {
+   var rectsides: Int
+   init(rectsides: Int) { self.rectsides = rectsides }
+}
+let objects: [AnyObject] = [Circle(radius: 2.0),result(area: 198),sides(rectsides: 4)]
+
+for object in objects {
+   if let objectWithArea = object as? rectangle {
+      print("Area is \(objectWithArea.area)")
+   } else {
+      print("Rectangle area is not defined")
+   }
+}
